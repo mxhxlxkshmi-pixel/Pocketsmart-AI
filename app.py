559363b6bb49b8158a2ea05a0d63745fa55eae9b
mxhxlxkshmi-pixel <mxhxlxkshmi@gmail.com>
2026-09-26 21:34:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Configure Gemini API
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 # ---------------------------------------------------------
 # PAGE ROUTES (Renders HTML templates)
